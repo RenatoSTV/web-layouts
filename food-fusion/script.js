ea65aa7f85e58f6c913716c697e94e0d7d962d45
Renato ST <renatostv@gmail.com>
2020-09-24 22:25:37 -0300

    alteração do script

--- food-fusion/script.js
+++ food-fusion/script.js
@@ -7,24 +7,25 @@ const categoryImg = document.querySelector('.category-image')
 for (let i = 0; i < categories.length; i++) {
     const categorySelected = categories[i]
     categorySelected.addEventListener('click', function () {
+        categoryImg.setAttribute('id', '')
 
+        requestAnimationFrame(() => {
+            for (let category of categories) {
+                for (let bgImg of bgImageCategory) {
+                    bgImg.classList.remove('active')
 
-        for (let category of categories) {
-            for (let bgImg of bgImageCategory) {
-                bgImg.classList.remove('active')
-
+                }
+                category.classList.remove('active')
             }
-            category.classList.remove('active')
-        }
 
 
-        let categoryId = categorySelected.getAttribute('id')
+            let categoryId = categorySelected.getAttribute('id')
 
-        categorySelected.classList.add('active')
-        bgImageCategory[i].classList.add('active')
+            categorySelected.classList.add('active')
+            bgImageCategory[i].classList.add('active')
 
-        categoryImg.setAttribute('id', 'show')
-        categoryImg.querySelector("img").src = `https://i.imgur.com/${categoryId}.png`
-
+            categoryImg.setAttribute('id', 'show')
+            categoryImg.querySelector("img").src = `https://i.imgur.com/${categoryId}.png`
+        })
     })
 }
